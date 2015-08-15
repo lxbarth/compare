eea2c6cef086d9519fb1ec2c206c998e0a91ea83
lxbarth <alex@mapbox.com>
2015-08-14 23:21:08 -0400

    Refactor into common.js.

--- common.js
+++ common.js
@@ -8,4 +8,48 @@ L.Yandex.prototype.getContainer =
 L.Google.prototype.getContainer = function() {
     return this._container;
 };
+
 L.mapbox.accessToken = 'pk.eyJ1IjoibHhiYXJ0aCIsImEiOiJFVXdYcUlvIn0.bbaHTEWlnAwGgyVwJngMdQ';
+
+var addLayer = function(layerid, map) {
+    var split = layerid.split('.');
+    var layer = null;
+    switch(split[0]) {
+        case 'bing':
+            layer = new L.BingLayer('AjCTNNlzpfcDOc0G58A4Hzx1N0OGrO8IXpFj1TVqlPG7sUxc8LqXbClnVK9RLk4q');
+            break;
+        case 'google':
+            // split[1] can be one of:
+            // ROADMAP
+            // SATELLITE
+            // HYBRID
+            // TERRAIN
+            layer = new L.Google(split[1] || 'ROADMAP');
+            break;
+        case 'yandex':
+            layer = new L.Yandex();
+            break;
+        case 'osm':
+            map.attributionControl.addAttribution('<a href="http://openstreetmap.org/copyright">&copy; OpenStreetMap contributors</a>');
+            layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+            break;
+        default:
+            layer = L.mapbox.tileLayer(layerid);
+            break;
+    }
+    // Remove classes Google.js adds.
+    var children = map.getContainer().children;
+    for (var i = 0; i < children.length; i++) {
+        children[i].className = children[i].className.replace(/\bleaflet-top\b/,'').replace(/\bleaflet-left\b/,'');
+    }
+    return layer.addTo(map);
+};
+
+var getLayerIds = function() {
+    if (!location.search) {
+        return null;
+    }
+    return (location.search.split('?')[1] || '')
+        .split('/')[0]
+        .split('&');
+};
--- swipe/site.js
+++ swipe/site.js
@@ -1,59 +1,18 @@
-L.Google.prototype.addTo =
-L.Yandex.prototype.addTo =
-function(map) {
-    map.addLayer(this);
-    return this;
-};
-L.Yandex.prototype.getContainer =
-L.Google.prototype.getContainer = function() {
-    return this._container;
-};
-L.mapbox.accessToken = 'pk.eyJ1IjoibHhiYXJ0aCIsImEiOiJFVXdYcUlvIn0.bbaHTEWlnAwGgyVwJngMdQ';
-
 (function() {
-    if (!location.search) {
+    var layerids = getLayerIds();
+    if (!layerids) {
         location.replace('..');
         return;
     }
-    var layerids = (location.search.split('?')[1] || '')
-        .split('/')[0]
-        .split('&');
 
-    var createLayer = function(layerid) {
-        var split = layerid.split('.');
-        switch(split[0]) {
-            case 'bing':
-                return new L.BingLayer('AjCTNNlzpfcDOc0G58A4Hzx1N0OGrO8IXpFj1TVqlPG7sUxc8LqXbClnVK9RLk4q');
-            case 'google':
-                // split[1] can be one of:
-                // ROADMAP
-                // SATELLITE
-                // HYBRID
-                // TERRAIN
-                return new L.Google(split[1] || 'ROADMAP');
-            case 'yandex':
-                return new L.Yandex();
-            case 'osm':
-                map.attributionControl.addAttribution('<a href="http://openstreetmap.org/copyright">&copy; OpenStreetMap contributors</a>');
-                return new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-            default:
-                return L.mapbox.tileLayer(layerid);
-        }
-    };
     var map = L.mapbox.map('map', null, {
         center: [0, 0],
         zoom: 3
     });
-    var hash = L.hash(map);
-    
-    var left = createLayer(layerids[0]).addTo(map);
-    var right = createLayer(layerids[1]).addTo(map);
+    L.hash(map);
 
-    // Remove classes Google.js adds.
-    left.getContainer().className =
-        left.getContainer().className.replace(/\bleaflet-top\b/,'').replace(/\bleaflet-left\b/,'');
-    right.getContainer().className =
-        right.getContainer().className.replace(/\bleaflet-top\b/,'').replace(/\bleaflet-left\b/,'');
+    var left = addLayer(layerids[0], map);
+    var right = addLayer(layerids[1], map);
 
     // Clip as you move map or range slider.
     var range = document.getElementById('range');
